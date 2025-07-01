28f6dbb3f86dba9b10f8b9e72b4569b1a7b229b8
masudaterutoshi <2415001t@stu.kobe-u.ac.jp>
2025-07-01 17:45:09 +0900

    swagger-add

--- backend/models/request.go
+++ backend/models/request.go
@@ -4,11 +4,11 @@ type CreateOrderRequest struct {
 	Products []OrderProductRequest `json:"products"`
 }
 type OrderProductRequest struct {
-	ProductID int `json:"product_id"`
-	Quantity  int `json:"quantity"`
+	ProductID int `json:"product_id" example:"1"`
+	Quantity  int `json:"quantity" example:"2"`
 }
 
 type AuthenticateRequest struct {
-	Email          string `json:"email"`
-	UserOrderToken string `json:"user_order_token,omitempty"`
+	Email          string `json:"email" example:"new.user@example.com"`
+	UserOrderToken string `json:"user_order_token,omitempty" example:"15ff4999-2cfd-41f3-b744-926e7c5c7a0e"`
 }
