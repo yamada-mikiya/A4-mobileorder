d67b1e571bf79d4d61b7715b1cf7cb0dbd5a50d8
honokakimura <2495090t@stu.kobe-u.ac.jp>
2025-08-18 23:27:29 +0900

    postcss.config.mjsの構成を修正し、Tailwind CSSプラグインの設定を追加

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {
+      config: "./tailwind.config.ts",
+    },
+  },
 };
-
-export default config;
